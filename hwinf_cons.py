ecca192cf799285f51f962c39e52789c99fa3a6a
alek_pol <a.polyanskiy@outlook.com>
2019-07-11 18:10:21 +0200

    Generate new Sheet for all organization

--- hwinf_cons.py
+++ hwinf_cons.py
@@ -16,6 +16,11 @@ def add_style(workbook, new_style, **kwargs):
 
 def init_style():
     b_side = Side(style='thin', color='000000')
+    add_style(new_wb, 'Header_0',
+              font=Font(name='Calibri', size=11, bold=True),
+              alignment=Alignment(vertical='center'),
+              )
+
     add_style(new_wb, 'Header',
               font=Font(name='Calibri', size=9, bold=True),
               alignment=Alignment(horizontal='center', vertical='center', wrap_text=True),
@@ -49,9 +54,36 @@ def init_style():
 
 # ===================================================
 if __name__ == '__main__':
+
+    src_path = 'c:\Development\Audit\src\*'
+
     # Create new excel file
     new_wb = openpyxl.Workbook()
     init_style()
+
+    # Create new Sheet - Raw
+    sncp = {'RAW': {'SheetName': 'RAW', 'CurrentPosition': '1'}}
+    if new_wb.sheetnames:
+        new_wb[new_wb.sheetnames[0]].title = 'Raw'
+        print()
+
+    new_wb.create_sheet(title='List', index=0)
+
+    org_path = glob.glob(src_path)
+    for i in range(len(org_path)):
+        org_name = org_path[i].split('\\')[-1]
+        if org_name not in sncp:
+            sncp[org_name] = {'SheetName': str(i + 1), 'CurrentPosition': '2'}
+
+            new_wb.create_sheet(title=sncp[org_name]['SheetName'], index=0)
+            sheet = new_wb[str(i+1)]
+            sheet['A1'] = org_name
+            sheet['A1'].style = 'Header_0'
+
+            # print(sncp[org_name]['SheetName'], org_name)
+
+
+
 
 
 
