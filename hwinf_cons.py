498a9f0eb0f2b0799160cc7b10427df662390a32
alek_pol <a.polyanskiy@outlook.com>
2019-07-11 17:25:16 +0200

    add def init 3 new style

--- hwinf_cons.py
+++ hwinf_cons.py
@@ -1,7 +1,10 @@
+import os
+import glob
 import openpyxl
 from bs4 import BeautifulSoup
 from openpyxl.styles import NamedStyle, Font, Border, Side, Alignment
 
+sncp = {} # sheet name and current positon
 
 def add_style(workbook, new_style, **kwargs):
     if new_style not in workbook.named_styles:
@@ -11,19 +14,7 @@ def add_style(workbook, new_style, **kwargs):
         workbook.add_named_style(n_style)
 
 
-# ===================================================
-if __name__ == '__main__':
-    print('--')
-
-
-# ---------------- Test ------------------------
-    # Create new excel file
-    new_wb = openpyxl.Workbook()
-    new_wb.create_sheet(title='RAW', index=0)
-    sheet = new_wb['RAW']
-
-
-
+def init_style():
     b_side = Side(style='thin', color='000000')
     add_style(new_wb, 'Header',
               font=Font(name='Calibri', size=9, bold=True),
@@ -31,8 +22,48 @@ if __name__ == '__main__':
               border=Border(left=b_side, top=b_side, right=b_side, bottom=b_side)
               )
 
-    sheet['A2'].style = 'Header'
-    sheet['A2'] = 'Test'
+    add_style(new_wb, 'Main_a_center',
+              font=Font(name='Calibri', size=9),
+              alignment=Alignment(horizontal='center', vertical='center', wrap_text=True),
+              border=Border(left=b_side, top=b_side, right=b_side, bottom=b_side)
+              )
 
+    add_style(new_wb, 'Main_a_left',
+              font=Font(name='Calibri', size=9),
+              alignment=Alignment(vertical='center', wrap_text=True),
+              border=Border(left=b_side, top=b_side, right=b_side, bottom=b_side)
+              )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# ===================================================
+if __name__ == '__main__':
+    # Create new excel file
+    new_wb = openpyxl.Workbook()
+    init_style()
+
+
+
+
+
+
+
+
+
+# ========================================
     new_wb.save('c:\Development\Audit\Test1.xlsx')
-    print('[Ok]')
+    print('Save file')
+
+
+
