6f80b6a4162e097cad45c335949091685e6dc185
alek_pol <a.polyanskiy@outlook.com>
2019-07-11 14:38:33 +0200

    add modules

---
+++ hwinf_cons.py
@@ -0,0 +1,10 @@
+import openpyxl
+from bs4 import BeautifulSoup
+from openpyxl.styles import NamedStyle, Font, Border, Side, Alignment
+
+
+# ===================================================
+if __name__ == '__main__':
+    print('--')
+
+
