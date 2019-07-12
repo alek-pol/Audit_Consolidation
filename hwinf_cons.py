9ea6358d319ffcb72c97e60ae43f136d66612554
alek_pol <a.polyanskiy@outlook.com>
2019-07-12 17:20:26 +0200

    renew out xlsx

--- hwinf_cons.py
+++ hwinf_cons.py
@@ -3,7 +3,7 @@ import glob
 import lxml
 import openpyxl
 from bs4 import BeautifulSoup
-from openpyxl.styles import NamedStyle, Font, Border, Side, Alignment
+from openpyxl.styles import NamedStyle, Font, Border, Side, Alignment, PatternFill
 
 src_path = 'c:\Development\Audit\src\*'
 dst_path = 'c:\Development\Audit\\'
@@ -32,6 +32,7 @@ def gen_cr(list_n):
     sncp[list_n]['CurCol'] += 1
     return cur_cr
 
+
 def add_style(workbook, new_style, **kwargs):
     if new_style not in workbook.named_styles:
         n_style = NamedStyle(name=new_style)
@@ -58,6 +59,27 @@ def init_style():
               font=Font(name='Calibri', size=9),
               alignment=Alignment(horizontal='center', vertical='center', wrap_text=True),
               border=Border(left=b_side, top=b_side, right=b_side, bottom=b_side)
+              )
+
+    add_style(new_wb, 'Main_center_red',
+              font=Font(name='Calibri', size=9),
+              alignment=Alignment(horizontal='center', vertical='center', wrap_text=True),
+              border=Border(left=b_side, top=b_side, right=b_side, bottom=b_side),
+              fill=PatternFill(fgColor="FFA07A", patternType='solid')
+              )
+
+    add_style(new_wb, 'Main_center_yellow',
+              font=Font(name='Calibri', size=9),
+              alignment=Alignment(horizontal='center', vertical='center', wrap_text=True),
+              border=Border(left=b_side, top=b_side, right=b_side, bottom=b_side),
+              fill=PatternFill(fgColor="FFE4B5", patternType='solid')
+              )
+
+    add_style(new_wb, 'Main_center_green',
+              font=Font(name='Calibri', size=9),
+              alignment=Alignment(horizontal='center', vertical='center', wrap_text=True),
+              border=Border(left=b_side, top=b_side, right=b_side, bottom=b_side),
+              fill=PatternFill(fgColor="98FB98", patternType='solid')
               )
 
     add_style(new_wb, 'Main_left',
@@ -102,45 +124,47 @@ def make_list():
             make_tab_body(new_wb['List'], 'B%s' % sncp['List']['CurRow'], hyperlink, 'Main_hyperlink')
 
 
+
+
+
 def make_raw_topic():
+    make_main_topic(new_wb['Raw'], 'A1', 'Общий свод данных', 'Topic_main')
 
+    sncp['Raw']['CurRow'] = 3
+    sncp['Raw']['CurCol'] = 0
+    make_tab_topic(new_wb['Raw'], gen_cr('Raw'), 'Наименование организации', 'Topic_tab', 30)
+    make_tab_topic(new_wb['Raw'], gen_cr('Raw'), 'Рабочее место', 'Topic_tab', 16)
+    make_tab_topic(new_wb['Raw'], gen_cr('Raw'), 'Название АРМ', 'Topic_tab', 10)
+    make_tab_topic(new_wb['Raw'], gen_cr('Raw'), 'Статус', 'Topic_tab', 10)
+    make_tab_topic(new_wb['Raw'], gen_cr('Raw'), 'Операционная система', 'Topic_tab', 20)
+    make_tab_topic(new_wb['Raw'], gen_cr('Raw'), 'Необходимость обновить ОС', 'Topic_tab', 15)
+    make_tab_topic(new_wb['Raw'], gen_cr('Raw'), 'Кол-во ядер', 'Topic_tab', 8)
+    make_tab_topic(new_wb['Raw'], gen_cr('Raw'), 'Кол-во логич-х проц-в', 'Topic_tab', 9)
+    make_tab_topic(new_wb['Raw'], gen_cr('Raw'), 'Процессор', 'Topic_tab', 20)
+    make_tab_topic(new_wb['Raw'], gen_cr('Raw'), 'Сокет', 'Topic_tab', 10)
+    make_tab_topic(new_wb['Raw'], gen_cr('Raw'), 'L3 - Кэш', 'Topic_tab', 10)
+    make_tab_topic(new_wb['Raw'], gen_cr('Raw'), 'Модель мат.платы', 'Topic_tab', 15)
+    make_tab_topic(new_wb['Raw'], gen_cr('Raw'), 'Год произв-ва', 'Topic_tab', 10)
 
-    make_main_topic(new_wb['Raw'], 'A1', 'Свод данных', 'Topic_main')
-
-
-    make_tab_topic(new_wb['Raw'], 'A3', 'Наименование организации', 'Topic_tab', 50)
-    make_tab_topic(new_wb['Raw'], 'B3', 'Рабочее место', 'Topic_tab', 20)
-    make_tab_topic(new_wb['Raw'], 'C3', 'Название АРМ', 'Topic_tab', 10)
-    make_tab_topic(new_wb['Raw'], 'D3', 'Операционная система', 'Topic_tab', 30)
-    make_tab_topic(new_wb['Raw'], 'E3', 'Кол-во ядер', 'Topic_tab', 8)
-    make_tab_topic(new_wb['Raw'], 'F3', 'Кол-во логич-х проц-в', 'Topic_tab', 9)
-    make_tab_topic(new_wb['Raw'], 'G3', 'Процессор', 'Topic_tab', 20)
-    make_tab_topic(new_wb['Raw'], 'H3', 'Сокет', 'Topic_tab', 10)
-    make_tab_topic(new_wb['Raw'], 'I3', 'L3 - Кэш', 'Topic_tab', 10)
-    make_tab_topic(new_wb['Raw'], 'J3', 'Модель мат.платы', 'Topic_tab', 15)
-    make_tab_topic(new_wb['Raw'], 'K3', 'Год произв-ва', 'Topic_tab', 10)
-
-
-
-
-
-    make_tab_topic(new_wb['Raw'], 'BX3', 'Пользователь', 'Topic_tab', 15)
-
+    #make_tab_topic(new_wb['Raw'], 'BX3', 'Пользователь', 'Topic_tab', 15)
     sncp['Raw']['CurRow'] = 4
 
 
 def make_org_topic(list_n, org_name):
     make_main_topic(new_wb[list_n], 'A1', org_name, 'Topic_main')
-    make_tab_topic(new_wb[list_n], 'A3', '№', 'Topic_tab', 5)
-    make_tab_topic(new_wb[list_n], 'B3', 'Рабочее место', 'Topic_tab', 18)
-    make_tab_topic(new_wb[list_n], 'C3', 'Операционная система', 'Topic_tab', 20)
-    make_tab_topic(new_wb[list_n], 'D3', 'Необходимо обновление ОС', 'Topic_tab', 13)
-    make_tab_topic(new_wb[list_n], 'E3', 'Процессор', 'Topic_tab', 15)
-    make_tab_topic(new_wb[list_n], 'F3', 'Кол-во ядер', 'Topic_tab', 8)
-    make_tab_topic(new_wb[list_n], 'G3', 'Сокет', 'Topic_tab', 8)
+
+    sncp[org_name]['CurRow'] = 3
+    sncp[org_name]['CurCol'] = 0
+    make_tab_topic(new_wb[list_n], gen_cr(org_name), '№', 'Topic_tab', 5)
+    make_tab_topic(new_wb[list_n], gen_cr(org_name), 'Рабочее место', 'Topic_tab', 16)
+    make_tab_topic(new_wb[list_n], gen_cr(org_name), 'Операционная система', 'Topic_tab', 20)
+    make_tab_topic(new_wb[list_n], gen_cr(org_name), 'Необходимо обновление ОС', 'Topic_tab', 13)
+    make_tab_topic(new_wb[list_n], gen_cr(org_name), 'Процессор', 'Topic_tab', 15)
+    make_tab_topic(new_wb[list_n], gen_cr(org_name), 'Кол-во ядер', 'Topic_tab', 8)
+    make_tab_topic(new_wb[list_n], gen_cr(org_name), 'Сокет', 'Topic_tab', 8)
 
 
-    sncp[org_name]['CurRow'] += 1
+    sncp[org_name]['CurRow'] = 4
 
 
 
@@ -181,6 +205,14 @@ def scan_hwi_htm(src_hwi_path):
         "BIOS Date:": ""
     }
 
+
+    socket_stat = {
+        '0': {}
+
+
+    }
+
+
     def make_headers_list(tables_n):
         headers = []
         for t in range(len(tables_n)):
@@ -206,38 +238,46 @@ def scan_hwi_htm(src_hwi_path):
                     flag_p = False
                     f_param[current_p] = tmp_s.rstrip().lstrip()
 
-    def check_os(os_n):
-        if os:
-            if os_n[2] not in ['10']:
-                return 'Да'
-            else:
-                if 'Home' in os_n:
-                    return 'Да'
-                elif int((os_n[int(os_n.index('Build')) + 1]).split('.')[0]) < min_win10_build:
-                    return 'Да'
-                else:
-                    return 'Нет'
+
+
 
     def check_date(date_m):
         date_m = int(date_m.split('/')[2])
         if int(date_m) < 100:
             date_m += 2000
-        return date_m
+        if date_m <= 2010:
+            return date_m, 0, 'Main_center_red'
+        elif date_m >= 2011 and date_m <= 2013:
+            return date_m, 1, 'Main_center_yellow'
+        else:
+            return date_m, 2, 'Main_center_green'
 
 
 
 
 
+
+    def check_os(os_n):
+        if os_n:
+            if os_n[2] not in ['10']:
+                return 'Да', 'Main_center_red'
+            else:
+                if 'Home' in os_n:
+                    return 'Да', 'Main_center_red'
+                elif int((os_n[int(os_n.index('Build')) + 1]).split('.')[0]) < min_win10_build:
+                    return 'Да', 'Main_center_red'
+                else:
+                    return 'Нет', 'Main_center_green'
+        return 'проверка', 'Main_center_yellow'
 
 
 
     # ====================
     header_list = make_headers_list(tables)
-    org_sn = sncp[org]['SheetName']
-    raw_cr = sncp['Raw']['CurRow']
-    org_cr = sncp[org]['CurRow']
+
     # ------------------------------------------------------------------------------------
     scan_value(tables[3], info_base)
+    oper_sys = (check_os(info_base["Operating System:"].split(' ')))
 
     index_cpu = header_list.index('Central Processor(s)')
     scan_value(tables[index_cpu + 1], info_cpu_1)
@@ -246,47 +286,44 @@ def scan_hwi_htm(src_hwi_path):
     # Motherboard info
     index_mb = header_list.index('Motherboard')
     scan_value(tables[index_mb + 1], info_mb)
+    date_status = check_date(info_mb["BIOS Date:"])
 
 
 
-
+    if date_status == 1:
+        status = ['на списание', '']
+    else:
+        status = ['', '']
 
 
 
     # entry Raw list
-    make_tab_body(new_wb['Raw'], 'A%s' % raw_cr, org, 'Main_left')
-    make_tab_body(new_wb['Raw'], 'B%s' % raw_cr, workplace, 'Main_left')
-    make_tab_body(new_wb['Raw'], 'C%s' % raw_cr, info_base["Computer Name:"], 'Main_left')
-    make_tab_body(new_wb['Raw'], 'D%s' % raw_cr, info_base["Operating System:"], 'Main_left')
-    make_tab_body(new_wb['Raw'], 'E%s' % raw_cr, info_cpu_1["Number Of Processor Cores:"], 'Main_center')
-    make_tab_body(new_wb['Raw'], 'F%s' % raw_cr, info_cpu_1["Number Of Logical Processors:"], 'Main_center')
-    make_tab_body(new_wb['Raw'], 'G%s' % raw_cr, info_cpu_2["CPU Brand Name:"], 'Main_center')
-    make_tab_body(new_wb['Raw'], 'H%s' % raw_cr, info_cpu_2["CPU Platform:"], 'Main_center')
-    make_tab_body(new_wb['Raw'], 'I%s' % raw_cr, info_cpu_2["L3 Cache:"], 'Main_center')
-    make_tab_body(new_wb['Raw'], 'J%s' % raw_cr, info_mb["Motherboard Model:"], 'Main_left')
-    make_tab_body(new_wb['Raw'], 'K%s' % raw_cr, check_date(info_mb["BIOS Date:"]), 'Main_center')
+    sncp['Raw']['CurCol'] = 0
+    make_tab_body(new_wb['Raw'], gen_cr('Raw'), org, 'Main_left')
+    make_tab_body(new_wb['Raw'], gen_cr('Raw'), workplace, 'Main_left')
+    make_tab_body(new_wb['Raw'], gen_cr('Raw'), info_base["Computer Name:"], 'Main_left')
+    make_tab_body(new_wb['Raw'], gen_cr('Raw'), status[0], 'Main_left')
+    make_tab_body(new_wb['Raw'], gen_cr('Raw'), info_base["Operating System:"], 'Main_left')
+    make_tab_body(new_wb['Raw'], gen_cr('Raw'), oper_sys[0], oper_sys[1])
+    make_tab_body(new_wb['Raw'], gen_cr('Raw'), info_cpu_1["Number Of Processor Cores:"], 'Main_center')
+    make_tab_body(new_wb['Raw'], gen_cr('Raw'), info_cpu_1["Number Of Logical Processors:"], 'Main_center')
+    make_tab_body(new_wb['Raw'], gen_cr('Raw'), info_cpu_2["CPU Brand Name:"], 'Main_center')
+    make_tab_body(new_wb['Raw'], gen_cr('Raw'), info_cpu_2["CPU Platform:"], 'Main_center')
+    make_tab_body(new_wb['Raw'], gen_cr('Raw'), info_cpu_2["L3 Cache:"], 'Main_center')
+    make_tab_body(new_wb['Raw'], gen_cr('Raw'), info_mb["Motherboard Model:"], 'Main_left')
+    make_tab_body(new_wb['Raw'], gen_cr('Raw'), date_status[0], date_status[2])
 
 
     # entry Org List
-    make_tab_body(new_wb[org_sn], 'A%s' % org_cr, org_cr - 3, 'Main_left')
-    make_tab_body(new_wb[org_sn], 'B%s' % org_cr, workplace, 'Main_left')
-    make_tab_body(new_wb[org_sn], 'C%s' % org_cr, info_base["Operating System:"], 'Main_left')
-    make_tab_body(new_wb[org_sn], 'D%s' % org_cr, check_os(info_base["Operating System:"].split(' ')), 'Main_center')
-    make_tab_body(new_wb[org_sn], 'E%s' % org_cr, info_cpu_2["CPU Brand Name:"], 'Main_left')
-    make_tab_body(new_wb[org_sn], 'F%s' % org_cr, info_cpu_1["Number Of Processor Cores:"], 'Main_left')
-    make_tab_body(new_wb[org_sn], 'G%s' % org_cr, info_cpu_2["L3 Cache:"], 'Main_left')
-
-
-
-    # -----------------------------------------------
-    # entry CPU info
-
-
-
-
-
-
-
+    org_sn = sncp[org]['SheetName']
+    sncp[org]['CurCol'] = 0
+    make_tab_body(new_wb[org_sn], gen_cr(org), 1, 'Main_left')
+    make_tab_body(new_wb[org_sn], gen_cr(org), workplace, 'Main_left')
+    make_tab_body(new_wb[org_sn], gen_cr(org), info_base["Operating System:"], 'Main_left')
+    make_tab_body(new_wb[org_sn], gen_cr(org), oper_sys[0], 'Main_center')
+    make_tab_body(new_wb[org_sn], gen_cr(org), info_cpu_2["CPU Brand Name:"], 'Main_left')
+    make_tab_body(new_wb[org_sn], gen_cr(org), info_cpu_1["Number Of Processor Cores:"], 'Main_left')
+    make_tab_body(new_wb[org_sn], gen_cr(org), info_cpu_2["L3 Cache:"], 'Main_left')
 
 
 
@@ -296,23 +333,15 @@ def scan_hwi_htm(src_hwi_path):
 
 
     # entry misk info
-
-    make_tab_body(new_wb['Raw'], 'BX%s' % sncp['Raw']['CurRow'], info_base["Current User Name:"], 'Main_left')
-
-
-
-
-
+    #make_tab_body(new_wb['Raw'], 'BX%s' % sncp['Raw']['CurRow'], info_base["Current User Name:"], 'Main_left')
     sncp['Raw']['CurRow'] += 1
     sncp[org]['CurRow'] += 1
 
 
-# Рейтинг тех. оснащенности
-#
-#
-#
-#
-#
+
+
+
+
 # ===================================================
 if __name__ == '__main__':
 
@@ -336,7 +365,7 @@ if __name__ == '__main__':
         new_wb.create_sheet(title=sncp[key]['SheetName'], index=0)
         if key not in sncp_lock:
             make_org_topic(sncp[key]['SheetName'], key)
-            sncp[key]['CurRow'] += 2
+            #sncp[key]['CurRow'] += 2
 
     make_list()  # Список листов с гиперссылкой
     make_raw_topic()
